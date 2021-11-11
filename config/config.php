10a04266d1bacdc2258d2d7d6fb749507b7246d2
TranQuanVinh <91666073+TranQuanVinh@users.noreply.github.com>
2021-11-11 16:54:00 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_HOST", "remotemysql.com");
-define("DB_USER", "FugpA3yQbB");
-define("DB_PASS", "dQrE8M064f");
-define("DB_NAME", "FugpA3yQbB");
+define("DB_USER", "9K68kWrt41");
+define("DB_PASS", "O5O9oCuBcc");
+define("DB_NAME", "9K68kWrt41");
 
 ?>
